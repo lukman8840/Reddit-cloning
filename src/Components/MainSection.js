d9865f21d97fe6fa5a732d79d931e72084f81993
lukman8840 <lukmansaid4280@gmail.com>
2024-08-03 23:40:09 +0100

    dropdown button added

--- src/Components/MainSection.js
+++ src/Components/MainSection.js
@@ -1,32 +1,38 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './Main.css';
 import { TiArrowDownOutline, TiArrowUpOutline } from "react-icons/ti";
-import { MdOutlineViewWeek } from "react-icons/md";
-import { LoremIpsum } from 'lorem-ipsum';
+import { MdOutlineViewWeek, MdOutlineEmojiEmotions } from "react-icons/md";
 import { FaRegMessage } from "react-icons/fa6";
 import { RiShareForwardLine } from "react-icons/ri";
-import { MdOutlineEmojiEmotions } from "react-icons/md";
 import { HiOutlineDotsHorizontal } from "react-icons/hi";
 import { FiCreditCard } from "react-icons/fi";
 import { FaRegRectangleList } from "react-icons/fa6";
-import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
-import { IoIosLink } from "react-icons/io";
+import { IoIosArrowDown, IoIosArrowUp, IoIosLink, IoIosCheckboxOutline } from "react-icons/io";
 import { TbArrowsSplit } from "react-icons/tb";
-import { IoIosCheckboxOutline } from "react-icons/io";
-
-const lorem = new LoremIpsum();
 
 const MainSection = () => {
   const [hoveredButton, setHoveredButton] = useState(null);
-  const [text, setText] = useState('');
+  const [posts, setPosts] = useState([]);
   const [activeButton, setActiveButton] = useState(null);
   const [selectedOption, setSelectedOption] = useState('Best');
-  const [voteCount, setVoteCount] = useState(0);
-  const [lastClickedArrow, setLastClickedArrow] = useState(null);
+  const [voteCounts, setVoteCounts] = useState({});
 
   useEffect(() => {
-    const generatedText = lorem.generateParagraphs(2);
-    setText(generatedText);
+    // Fetch posts from Reddit API
+    axios.get('https://www.reddit.com/r/reactjs/hot.json')
+      .then(response => {
+        const fetchedPosts = response.data.data.children.map(child => child.data);
+        const initialVoteCounts = {};
+        fetchedPosts.forEach(post => {
+          initialVoteCounts[post.id] = 0; // Initialize vote count for each post
+        });
+        setPosts(fetchedPosts);
+        setVoteCounts(initialVoteCounts);
+      })
+      .catch(error => {
+        console.error("Error fetching data from Reddit API:", error);
+      });
   }, []);
 
   const handleClick = (button) => () => {
@@ -46,15 +52,19 @@ const MainSection = () => {
     setActiveButton(null)
   };
 
-  const incrementVote = () => {
-    setVoteCount(voteCount + 1);
-    setLastClickedArrow('up');
+  const incrementVote = (postId) => {
+    setVoteCounts(prevVoteCounts => ({
+      ...prevVoteCounts,
+      [postId]: prevVoteCounts[postId] + 1
+    }));
   };
 
-  const decrementVote = (event) => {
+  const decrementVote = (event, postId) => {
     event.stopPropagation();
-    setVoteCount(voteCount - 1);
-    setLastClickedArrow('down');
+    setVoteCounts(prevVoteCounts => ({
+      ...prevVoteCounts,
+      [postId]: prevVoteCounts[postId] - 1
+    }));
   };
 
   const buttonStyle = (button) => ({
@@ -102,178 +112,72 @@ const MainSection = () => {
       </div>
       <hr className='hr' />
       <div className='main-content'>
-        <div className='content-header'>
-          <img src='assets/header.jpeg' alt='Header' />
-          <p className='paragraph'>Header Paragraph &bull;</p>
-          <span className='time'>14 hr ago</span>
-          <HiOutlineDotsHorizontal style={{ 
-            color: '#fff',
-            cursor: 'pointer',
-            marginLeft: '450px'
-          }}/>
-        </div>
-        
-        <div className='content-main'>
-          <h1>This is a Header of the Text</h1>
-          <p className='text'>{text}</p>
-        </div>
-      </div>
-      <div className='footer-btn'>
-        <button
-          style={buttonStyle('vote')}
-          onMouseEnter={() => handleMouseEnter('vote')}
-          onMouseLeave={handleMouseLeave}
-          onClick={incrementVote}
-        >
-          <TiArrowUpOutline style= {{ 
-            fontSize: '20px',
+        {posts.map((post, index) => (
+          <div key={index} className='post'>
+            <div className='content-header'>
+              <img src={post.thumbnail} alt='Header' />
+              <p className='paragraph'>{post.title} &bull;</p>
+              <span className='time'>{new Date(post.created_utc * 1000).toLocaleTimeString()}</span>
+              <HiOutlineDotsHorizontal style={{ 
+                color: '#fff',
+                cursor: 'pointer',
+                marginLeft: '450px'
+              }}/>
+            </div>
             
-          }} 
-        />
-          <span style={{ 
-            marginLeft: '5px', 
-            marginRight: '5px' 
-            }}>{voteCount}</span>
-          <TiArrowDownOutline style={{ 
-            fontSize: '20px' }}
-            onClick={decrementVote}
-        />
-        </button>
-        <button
-          className='comments'
-          style={buttonStyle('comments')}
-          onMouseEnter={() => handleMouseEnter('comments')}
-          onMouseLeave={handleMouseLeave}
-        >
-          <FaRegMessage style={{ 
-            fontSize: '16px', 
-            margin: '5px' 
-            }} 
-        /> 10 
-        </button>
-        <button
-          className='comments'
-          style={buttonStyle('emojis')}
-          onMouseEnter={() => handleMouseEnter('emojis')}
-          onMouseLeave={handleMouseLeave}
-        >
-          <MdOutlineEmojiEmotions />
-        </button>
-        <button
-          className='comments'
-          style={buttonStyle('share')}
-          onMouseEnter={() => handleMouseEnter('share')}
-          onMouseLeave={handleMouseLeave}
-          onClick={handleClick('share')}
-        >
-          <RiShareForwardLine style={{ 
-            fontSize: '20px', 
-            margin: '5px' 
-        }} 
-        /> 
-          share
-        </button>
-      </div>
-        <hr style={{
-          margin: '10px',
-        }}/>
-        {activeButton === 'share' && (
-          <div className='dropdown-header-3'>
-            <h4>Share this Post on</h4>
-              <p onClick={() => setActiveButton('copy link')}> <IoIosLink /> Copy link</p>
-              <p onClick={() => setActiveButton('crosspost')}> <TbArrowsSplit /> Crosspost</p>
-              <p onClick={() => setActiveButton('embed')}> <IoIosCheckboxOutline /> Embed</p>
+            <div className='content-main'>
+              <h1>{post.title}</h1>
+              <p className='text'>{post.selftext}</p>
+            </div>
+            <div className='footer-btn'>
+              <button
+                style={buttonStyle('vote')}
+                onMouseEnter={() => handleMouseEnter('vote')}
+                onMouseLeave={handleMouseLeave}
+                onClick={() => incrementVote(post.id)}
+              >
+                <TiArrowUpOutline style={{ fontSize: '20px' }} />
+                <span style={{ marginLeft: '5px', marginRight: '5px' }}>{voteCounts[post.id]}</span>
+                <TiArrowDownOutline style={{ fontSize: '20px' }} onClick={(event) => decrementVote(event, post.id)} />
+              </button>
+              <button
+                className='comments'
+                style={buttonStyle('comments')}
+                onMouseEnter={() => handleMouseEnter('comments')}
+                onMouseLeave={handleMouseLeave}
+              >
+                <FaRegMessage style={{ fontSize: '16px', margin: '5px' }} /> {post.num_comments}
+              </button>
+              <button
+                className='comments'
+                style={buttonStyle('emojis')}
+                onMouseEnter={() => handleMouseEnter('emojis')}
+                onMouseLeave={handleMouseLeave}
+              >
+                <MdOutlineEmojiEmotions />
+              </button>
+              <button
+                className='comments'
+                style={buttonStyle('share')}
+                onMouseEnter={() => handleMouseEnter('share')}
+                onMouseLeave={handleMouseLeave}
+                onClick={handleClick('share')}
+              >
+                <RiShareForwardLine style={{ fontSize: '20px', margin: '5px' }} /> share
+              </button>
+            </div>
+            <hr style={{ margin: '10px' }}/>
+            {activeButton === 'share' && (
+              <div className='dropdown-header-3'>
+                <h4>Share this Post on</h4>
+                <p onClick={() => setActiveButton('copy link')}> <IoIosLink /> Copy link</p>
+                <p onClick={() => setActiveButton('crosspost')}> <TbArrowsSplit /> Crosspost</p>
+                <p onClick={() => setActiveButton('embed')}> <IoIosCheckboxOutline /> Embed</p>
+              </div>
+            )}
           </div>
-        )}
-      <div className='main-content'>
-        <div className='content-header'>
-          <img src='assets/header.jpeg' alt='Header' />
-          <p className='paragraph'>Header Paragraph &bull;</p>
-          <span className='time'>14 hr ago</span>
-          <HiOutlineDotsHorizontal style={{ 
-            color: '#fff',
-            cursor: 'pointer',
-            marginLeft: '450px'
-          }}/>
-        </div>
-        
-        <div className='content-main'>
-          <h1>This is a Header of the Text</h1>
-          <img src='assets/bg.jpeg' style={{
-            width: '100%',
-            height: '300px',
-            objectFit: 'cover', 
-            borderRadius: '10px',
-            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-          }} />
-        </div>
+        ))}
       </div>
-      <div className='footer-btn'>
-        <button
-          style={buttonStyle('vote')}
-          onMouseEnter={() => handleMouseEnter('vote')}
-          onMouseLeave={handleMouseLeave}
-        >
-          <TiArrowUpOutline style={{ 
-            fontSize: '20px' 
-          }} 
-          onClick={incrementVote}
-        />
-          <span style={{ 
-            marginLeft: '5px', 
-            marginRight: '5px' 
-            }}>{voteCount}</span>
-          <TiArrowDownOutline style={{ 
-            fontSize: '20px' }}
-            onClick={decrementVote}
-        />
-        </button>
-        <button
-          className='comments'
-          style={buttonStyle('comments')}
-          onMouseEnter={() => handleMouseEnter('comments')}
-          onMouseLeave={handleMouseLeave}
-        >
-          <FaRegMessage style={{ 
-            fontSize: '16px', 
-            margin: '5px' 
-            }} 
-        /> 100 
-        </button>
-        <button
-          className='comments'
-          style={buttonStyle('emojis')}
-          onMouseEnter={() => handleMouseEnter('emojis')}
-          onMouseLeave={handleMouseLeave}
-        >
-          <MdOutlineEmojiEmotions />
-        </button>
-        <button
-          className='comments'
-          style={buttonStyle('share')}
-          onMouseEnter={() => handleMouseEnter('share')}
-          onMouseLeave={handleMouseLeave}
-          onClick={handleClick('share')}
-        >
-          <RiShareForwardLine style={{ 
-            fontSize: '20px', 
-            margin: '5px' 
-        }} 
-        /> 
-          share
-        </button>
-      </div>
-      <hr style={{
-          margin: '10px',
-        }}/>
-        {activeButton === 'share' && (
-          <div className='dropdown-header-3'>
-            <h4>Share this Post on</h4>
-              <p onClick={() => setActiveButton('copy link')}> <IoIosLink /> Copy link</p>
-              <p onClick={() => setActiveButton('crosspost')}> <TbArrowsSplit /> Crosspost</p>
-              <p onClick={() => setActiveButton('embed')}> <IoIosCheckboxOutline /> Embed</p>
-          </div>
-        )}
     </div>
   );
 };
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import { FaReddit, FaSearch, FaBell, FaUserCircle, FaPlus } from 'react-icons/fa';
 import { CiSearch } from "react-icons/ci";
 import { FaRegBell } from "react-icons/fa";
 import { AiOutlineMessage } from "react-icons/ai";
 import { MdAdsClick } from "react-icons/md";
-
+import { IoShirtOutline } from "react-icons/io5";
+import { IoLogInOutline } from "react-icons/io5";
+import { PiBag } from "react-icons/pi";
+import { IoSettingsOutline } from "react-icons/io5";
+import { TbPremiumRights } from "react-icons/tb";
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Navbar = () => {
+    const [isCartVisible, setIsCartVisible] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    const handleProfileClick = () => {
+        setIsCartVisible(!isCartVisible);
+    };
+
+    const handleDarkModeClick = () => {
+        setIsDarkMode(!isDarkMode);
+    };
+
     return (
         <nav className="navbar">
             <div className="navbar-left">
-                <img src='assets/icon.jpg' />
+                <img src='assets/icon.jpg' alt="Logo" />
                 <div className='navbar-title-container'>
-                <span className="navbar-title">reddit</span>
-                <div className='tooltip'>Go back to reddit Home</div>
+                    <span className="navbar-title">reddit</span>
+                    <div className='tooltip'>Go back to reddit Home</div>
                 </div>
             </div>
             <div className="navbar-center">
@@ -27,12 +44,59 @@ const Navbar = () => {
                 <MdAdsClick className='navbar-icon'/>
                 <AiOutlineMessage className='navbar-icon message'/>
                 <p className='navbar-icon'>
-                  <FaPlus style={{ fontSize: '15px'}} />
+                    <FaPlus style={{ fontSize: '15px'}} />
                     Create
-                    </p>
-
+                </p>
                 <FaRegBell className='navbar-icon'/>
-                <FaUserCircle className="navbar-icon user" />
+                <FaUserCircle className="navbar-icon user" onClick={handleProfileClick} />
+                <FontAwesomeIcon 
+                    icon={isDarkMode ? faSun : faMoon} 
+                    className={`navbar-icon dark-mode-icon ${isDarkMode ? 'flipped' : ''}`} 
+                    onClick={handleDarkModeClick} 
+                />
+                {isCartVisible && (
+                    <div className='cart-dropdown'>
+                        <ul>
+                            <li>
+                                <FaUserCircle />
+                                View Profile
+                                <span>u/Low-Tangelo-4110</span>
+                            </li>
+                            <li>
+                                <IoShirtOutline />
+                                Edit Avatar
+                            </li>
+                            <li>
+                                <PiBag />
+                                Contributor Program
+                                <span>0 gold earned</span>
+                            </li>
+                            <li>
+                                <FontAwesomeIcon icon={faMoon} />
+                                Dark Mode
+                            </li>
+                            <li>
+                                <IoLogInOutline />
+                                Logout
+                            </li>
+                            <hr />
+                            <li>
+                                <MdAdsClick />
+                                Advertise on Reddit
+                            </li>
+                            <hr />
+                            <li>
+                                <IoSettingsOutline />
+                                Settings
+                            </li>
+                            <hr />
+                            <li>
+                                <TbPremiumRights />
+                                Premium
+                            </li>
+                        </ul>
+                    </div>
+                )}
             </div>
         </nav>
     );
